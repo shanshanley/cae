33998f0a6f6564671a3554d1d623411f59757810
shanshanley <98719942+shanshanley@users.noreply.github.com>
2022-02-26 02:38:05 +0800

    Update and rename README.md to index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<body bgcolor="#6BA961">
+<td width="33.33%" valign="top">
+<table border="0" width="100%" cellpadding="15" cellspacing="0" align="center" bgcolor="#353535">
+<tr> <td> 
+<font face="arial" size="5" color="#ffffff">
+<center> Performance Task in CAE 9 </font> 
+<br> <br> 
+<br> <br>
+<a href="https://docs.google.com/document/d/16eFgKF76daCSg8H1YcQxUv1EF_juUN6C3FQ5RqWwBjA/edit?usp=sharing"> <font face="arial" size="4" color="#03EE5C"> 
+Clink here to access the Critique Paper
+</center> </font> <br> <br>
+</td> </tr> </table> </td> </html>
